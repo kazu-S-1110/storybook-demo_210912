9dbd82d77ae3b84fbba1e1a719442df925b69ac9
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-12 17:43:09 +0900

    おお、サーバが起動してなくても確認ができた。結構良さげ？公式マニュアルが少し読みづらいな。。

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,11 @@
+import '../src/index.css'; //👈 The app's CSS file goes here
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
